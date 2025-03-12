9d9c845843dc77abd46d22ae3e05cc367b8c95e7
JimenaG02 <jimena.garcia@cua.uam.mx>
2025-03-12 02:01:02 -0600

    practica1.py

--- practica1.py
+++ practica1.py
@@ -33,19 +33,31 @@ sueldo_bruto = 0.0
 
 if horas <= 160:
     sueldo_bruto = salarioxhr * horas
+
 elif 160 < horas <= 200:
+    # Calcula las horas excedentes a 160
     horas_excedente = horas - 160
     sueldo_bruto = (160 * salarioxhr) + (horas_excedente * salarioxhr * 1.5)
+    # Sueldo bruto: Las primeras 160 horas se pagan al salario normal y las horas excedentes se pagan
+
 else:
+    # Primera parte: 160 horas al salario normal
     sueldo_normal = 160 * salarioxhr
+
+    # Segunda parte: 40 horas (de 161 a 200) a 1.5 veces el salario
     sueldo_excedente161 = 40 * (salarioxhr * 1.5)
+
+    # Tercera parte: horas excedentes a 200 al doble del salario
     horas_excedentes200 = horas - 200
     sueldo_extra_200 = horas_excedentes200 * (salarioxhr * 2)
+
+    # Sueldo bruto total
     sueldo_bruto = sueldo_normal + sueldo_excedente161 + sueldo_extra_200
 
 print("\nSueldo bruto:", sueldo_bruto)
 
 # ISR
+#Asignamos valores a la cuota, limite inferior y porcentaje excedente para poder calcular el ISR dependiendo de su rango
 if 0.01 <= sueldo_bruto <= 746.04:
     limite_inferior = 0.01
     cuota = 0
@@ -90,33 +102,34 @@ else:
     limite_inferior = 375975.62
     cuota = 117912.32
     porcentaje_excedente = 35
-
+#formula ISR y se lo restamos al sueldo neto
 isr = cuota + (sueldo_bruto - limite_inferior) * (porcentaje_excedente / 100)
 sueldo_neto = sueldo_bruto - isr
 
 print("ISR:", isr)
 
 # Seguro Social
-aportacion_ss = sueldo_bruto * 0.025
+aportacion_ss = sueldo_bruto * 0.025 #2.5% = 0.025
 sueldo_neto -= aportacion_ss
 print("Aportación Seguro Social:", aportacion_ss,"\n")
 
 # Caja de Ahorro
 print("<| Caja de ahorro |>")
 op = int(input("¿Qué tipo de cuota tienes?\n1. Cuota fija\n2. Cuota porcentual del 3%\n3. Cuota del 5%\n"))
-if op == 1:
+if op == 1: #cuota fija
     sueldo_neto -= 1000
-elif op == 2:
-    sueldo_neto -= sueldo_bruto * 0.03
-elif op == 3:
-    sueldo_neto -= sueldo_bruto * 0.05
+elif op == 2: #Cuota porcentual 3%
+    sueldo_neto -= sueldo_bruto * 0.03 #3% = 0.03
+elif op == 3: # Cuota del 5%
+    sueldo_neto -= sueldo_bruto * 0.05 #5% = 0.05
 
 # Ahorro para el retiro
 print("<| Ahorro para el retiro |>")
 op2 = int(input("Elige alguna de las siguientes opciones:\n1. No participar\n2. Aportar 1%\n3. Aportar 2%\n"))
-if op2 == 2:
+#No hay op2==1 debido a que los valores del sueldo neto no se modifican
+if op2 == 2: #Aportar 1%
     sueldo_neto -= sueldo_neto * 0.01
-elif op2 == 3:
+elif op2 == 3: #Aportar 2%
     sueldo_neto -= sueldo_neto * 0.02
 
-print("\nSueldo neto final: $", sueldo_neto)
+print("\nSueldo neto final: $",sueldo_neto)
